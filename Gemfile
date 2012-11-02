e72923ced69eba15f90c8d4cb3f23ee9b597dc2a
Rafał Rzepecki <divided.mind@gmail.com>
2012-11-02 23:53:46 +0100

    Spec for behaviour when encountering an unexpected instance type.

--- Gemfile
+++ Gemfile
@@ -2,4 +2,8 @@
 source "https://rubygems.org"
 gemspec
 
+group :test do
+  gem 'rspec', '~> 2.11.0'
+end
+
 # gem "rails"
---
+++ spec/price_list_spec.rb
@@ -0,0 +1,39 @@
+require 'amazon-pricing'
+
+describe AwsPricing::PriceList do
+  describe "#get_ec2_on_demand_instance_pricing" do
+    let(:price_list) {
+      {
+        'config' => {
+          'regions' => [{
+            'region' => 'us-east',
+            'instanceTypes' => [{
+              'type' => 'stdODI',
+              'sizes' => [{
+                'size' => 'sm',
+                'valueColumns' => [
+                  {'name' => 'linux', 'prices' => { 'USD' => '0.065' }},
+                  {'name' => 'mswin', 'prices' => { 'USD' => '0.265' }}
+                                  ]}]}]}]
+        }
+      }
+    }
+
+    before do
+      AwsPricing::PriceList.any_instance.tap do |it|
+        it.stub :get_ec2_reserved_instance_pricing
+        it.stub :fetch_ec2_ebs_pricing
+        it.stub :fetch_url => price_list
+      end
+    end
+    
+    it "doesn't break when amazon adds instance types" do
+      newType = {'type' => 'quantumODI', 'sizes' => [{'size' => 'sm', 'valueColumns' => []}]}
+      price_list['config']['regions'][0]['instanceTypes'] << newType
+
+      expect {
+        AwsPricing::PriceList.new
+      }.to_not raise_error
+    end
+  end
+end
