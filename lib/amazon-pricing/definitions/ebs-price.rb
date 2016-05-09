99cbeafe871c6d1cda91bf817e17784092f92128
BJORN BERGSTEN <bjorn@BJORNs-MacBook-Pro-2.local>
2016-05-09 18:36:09 +0000

    new aws volume types: read JSON

--- lib/amazon-pricing/definitions/ebs-price.rb
+++ lib/amazon-pricing/definitions/ebs-price.rb
@@ -12,7 +12,8 @@ module AwsPricing
   class EbsPrice
     attr_accessor :standard_per_gb, :standard_per_million_io, :s3_snaps_per_gb,
       :preferred_per_gb, :preferred_per_iops,
-      :ssd_per_gb
+      :ssd_per_gb,
+      :ebs_cold_hdd_per_gb, :ebs_optimized_hdd_per_gb
 
     def initialize(region)
       #@region = region
@@ -22,14 +23,18 @@ module AwsPricing
     def update_from_json(json)
       json["types"].each do |t|
         case t["name"]
-        when "Amazon EBS Magnetic volumes"
+        when "Amazon EBS Magnetic volumes"  # not supported by aws anymore, replaced by st1 and sc1
           @standard_per_gb = t["values"].select{|v| v["rate"] == "perGBmoProvStorage" }.first["prices"].values.first.to_f
           @standard_per_million_io = t["values"].select{|v| v["rate"] == "perMMIOreq" }.first["prices"].values.first.to_f
-        when "Amazon EBS Provisioned IOPS (SSD) volumes"
+        when "Amazon EBS Provisioned IOPS SSD (io1) volumes"
           @preferred_per_gb = t["values"].select{|v| v["rate"] == "perGBmoProvStorage" }.first["prices"].values.first.to_f
           @preferred_per_iops = t["values"].select{|v| v["rate"] == "perPIOPSreq" }.first["prices"].values.first.to_f
-        when "Amazon EBS General Purpose (SSD) volumes"
+        when "Amazon EBS General Purpose SSD (gp2) volumes"
           @ssd_per_gb = t["values"].select{|v| v["rate"] == "perGBmoProvStorage" }.first["prices"].values.first.to_f
+        when "Amazon EBS Cold HDD (sc1) volumes"
+          @ebs_optimized_hdd_per_gb = t["values"].select{|v| v["rate"] == "perGBmoProvStorage" }.first["prices"].values.first.to_f
+        when "Amazon EBS Throughput Optimized HDD (st1) volumes"
+          @ebs_cold_hdd_per_gb = t["values"].select{|v| v["rate"] == "perGBmoDataStored" }.first["prices"].values.first.to_f
         when "ebsSnapsToS3"
           @s3_snaps_per_gb = t["values"].select{|v| v["rate"] == "perGBmoDataStored" }.first["prices"].values.first.to_f
         end
