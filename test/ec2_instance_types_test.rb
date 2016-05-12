f0a4b071d2e9b33d7bf245b794c4761722d874ac
BJORN BERGSTEN <bjorn@BJORNs-MacBook-Pro-2.local>
2016-05-12 16:52:46 +0000

    typo

--- test/ec2_instance_types_test.rb
+++ test/ec2_instance_types_test.rb
@@ -88,8 +88,8 @@ class TestEc2InstanceTypes < Test::Unit::TestCase
     assert region.ebs_price.preferred_per_iops == 0.065
     assert region.ebs_price.s3_snaps_per_gb == 0.095
     # next two prices were added by aws (May 09, 2016)
-    assert region.ebs_price.ebs_optimized_hhd_per_gb == 0.045
-    assert region.ebs_price.ebs_cold_hhd_per_gb == 0.025
+    assert region.ebs_price.ebs_optimized_hdd_per_gb == 0.045
+    assert region.ebs_price.ebs_cold_hdd_per_gb == 0.025
 
   end
 
@@ -101,8 +101,8 @@ class TestEc2InstanceTypes < Test::Unit::TestCase
       assert_not_nil region.ebs_price.standard_per_million_io
       assert_not_nil region.ebs_price.preferred_per_gb
       assert_not_nil region.ebs_price.preferred_per_iops
-      assert_not_nil region.ebs_price.ebs_optimized_hhd_per_gb
-      assert_not_nil region.ebs_price.ebs_cold_hhd_per_gb
+      assert_not_nil region.ebs_price.ebs_optimized_hdd_per_gb
+      assert_not_nil region.ebs_price.ebs_cold_hdd_per_gb
       assert_not_nil region.ebs_price.s3_snaps_per_gb
     end
   end
@@ -183,8 +183,8 @@ class TestEc2InstanceTypes < Test::Unit::TestCase
     assert region.ebs_price.preferred_per_iops == 0.078
     assert region.ebs_price.s3_snaps_per_gb == 0.125
     # next two prices were added by aws (May 09, 2016)
-    assert region.ebs_price.ebs_optimized_hhd_per_gb == 0.054
-    assert region.ebs_price.ebs_cold_hhd_per_gb == 0.03
+    assert region.ebs_price.ebs_optimized_hd_per_gb == 0.054
+    assert region.ebs_price.ebs_cold_hdd_per_dgb == 0.03
 
   end
 
