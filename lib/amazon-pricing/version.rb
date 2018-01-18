8b111a3a25d4280ac5fb68d34dbaa669640ff0b5
Francesca Nannizzi <fnannizzi@gmail.com>
2018-01-18 15:59:57 +0000

    Bump amazon-pricing version.

--- lib/amazon-pricing/version.rb
+++ lib/amazon-pricing/version.rb
@@ -8,5 +8,5 @@
 # Home::      http://github.com/CloudHealth/amazon-pricing
 #++
 module AwsPricing
-VERSION = '0.1.106' # [major,minor.fix]: adding eu-west-3
+VERSION = '0.1.107' # [major,minor.fix]: adding more methods for rds sf
 end
