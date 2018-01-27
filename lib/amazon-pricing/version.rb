adbe01a32eb4926292191b6d1195807d55d35e11
Stan Luke <stan@cloudhealthtech.com>
2018-01-27 11:38:20 +0000

    bump version for db_mapping extra-space fix

--- lib/amazon-pricing/version.rb
+++ lib/amazon-pricing/version.rb
@@ -8,5 +8,5 @@
 # Home::      http://github.com/CloudHealth/amazon-pricing
 #++
 module AwsPricing
-VERSION = '0.1.107' # [major,minor.fix]: adding more methods for rds sf
+VERSION = '0.1.108' # [major,minor.fix]: adding more methods for rds sf
 end
