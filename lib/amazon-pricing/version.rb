6365bc93ecfed2b0e977409849bb19a3fdaf6761
Natahlie Beavers <nbeavers@cloudhealthtech.com>
2018-07-31 14:50:09 +0000

    Adding 6xlarge, updating version number

--- lib/amazon-pricing/version.rb
+++ lib/amazon-pricing/version.rb
@@ -8,5 +8,5 @@
 # Home::      http://github.com/CloudHealth/amazon-pricing
 #++
 module AwsPricing
-VERSION = '0.1.116' # [major,minor.fix]: adding z1D instances
+VERSION = '0.1.117' # [major,minor.fix]: adding z1D, r5, r5d, sbe1 instances
 end
