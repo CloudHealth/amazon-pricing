cbae3c56d13f09772002ed89ed8c7c9b71937e2d
Natahlie Beavers <nbeavers@cloudhealthtech.com>
2018-10-11 18:39:05 +0000

    Bumping version again to reflect correct value

--- lib/amazon-pricing/version.rb
+++ lib/amazon-pricing/version.rb
@@ -8,5 +8,5 @@
 # Home::      http://github.com/CloudHealth/amazon-pricing
 #++
 module AwsPricing
-VERSION = '0.1.119' # [major,minor.fix]: adding osaka region
+VERSION = '0.1.120' # [major,minor.fix]: adding osaka region
 end
