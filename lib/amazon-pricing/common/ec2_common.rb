4fe3a709a7edaad8b875b0733517167d1b4a9817
Natahlie Beavers <nbeavers@cloudhealthtech.com>
2018-10-15 15:09:23 +0000

    Trying again to fix test

--- lib/amazon-pricing/common/ec2_common.rb
+++ lib/amazon-pricing/common/ec2_common.rb
@@ -124,7 +124,7 @@ module AwsPricing
             end
             price = od_option["prices"]["USD"]
             instance_type.update_pricing_new(operating_system, :ondemand, price)
-            Rails.logger.debug "od pricing update #{api_name} price #{price} for #{region_name}/#{operating_system}"
+            Logger.debug "od pricing update #{api_name} price #{price} for #{region_name}/#{operating_system}"
             # prevent iteration, since it doesn't make sense, noting it's (theoretically) possible
             break
           end
