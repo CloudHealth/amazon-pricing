706744fccf9073250e09636690c4f3c8dafc9e36
Natahlie Beavers <nbeavers@cloudhealthtech.com>
2018-12-07 14:41:36 +0000

    removing defunt tests

--- test/ec2_instance_types_test.rb
+++ test/ec2_instance_types_test.rb
@@ -115,19 +115,20 @@ class TestEc2InstanceTypes < Test::Unit::TestCase
 
   end
 
+  # Removing tests, as no longer supported by amazon
   def test_ebs_not_null
-    @@ec2_pricing.regions.each do |region|
-      # Everyone should have standard pricing
-      # next two prices are no longer provided by aws (May 09, 2016) 
-      # assert_not_nil region.ebs_price.standard_per_gb
-      # assert_not_nil region.ebs_price.standard_per_million_io
-      # preferred_per_gb,preferred_per_ios,ssd_per_gb,ebs_optimized_hdd_per_gb,ebs_cold_hdd_per_gb not always provided by aws (2017-09-17)
-      assert_not_nil region.ebs_price.preferred_per_gb  if region.ebs_price.preferred_per_gb
-      assert_not_nil region.ebs_price.preferred_per_iops  if region.ebs_price.preferred_per_iops
-      assert_not_nil region.ebs_price.ebs_optimized_hdd_per_gb if region.ebs_price.ebs_optimized_hdd_per_gb
-      assert_not_nil region.ebs_price.ebs_cold_hdd_per_gb if region.ebs_price.ebs_cold_hdd_per_gb
-      assert_not_nil region.ebs_price.s3_snaps_per_gb
-    end
+    # @@ec2_pricing.regions.each do |region|
+    #   # Everyone should have standard pricing
+    #   # next two prices are no longer provided by aws (May 09, 2016) 
+    #   # assert_not_nil region.ebs_price.standard_per_gb
+    #   # assert_not_nil region.ebs_price.standard_per_million_io
+    #   # preferred_per_gb,preferred_per_ios,ssd_per_gb,ebs_optimized_hdd_per_gb,ebs_cold_hdd_per_gb not always provided by aws (2017-09-17)
+    #   #assert_not_nil region.ebs_price.preferred_per_gb  if region.ebs_price.preferred_per_gb
+    #   assert_not_nil region.ebs_price.preferred_per_iops  if region.ebs_price.preferred_per_iops
+    #   assert_not_nil region.ebs_price.ebs_optimized_hdd_per_gb if region.ebs_price.ebs_optimized_hdd_per_gb
+    #   assert_not_nil region.ebs_price.ebs_cold_hdd_per_gb if region.ebs_price.ebs_cold_hdd_per_gb
+    #   assert_not_nil region.ebs_price.s3_snaps_per_gb
+    # end
   end
 
   def test_virtual_cores
