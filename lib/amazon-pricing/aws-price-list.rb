8f09a5b67f7ec578670acee6ce9f94a7d767ab09
Natahlie Wang <nbeavers@cloudhealthtech.com>
2019-04-25 15:41:41 +0000

    AWS-4815: Adding support for new hong kong region

--- lib/amazon-pricing/aws-price-list.rb
+++ lib/amazon-pricing/aws-price-list.rb
@@ -23,7 +23,7 @@ module AwsPricing
       # Creating regions upfront since different json files all use different naming conventions. No more ad-hoc creation.
       regions = ["eu-west-1", "sa-east-1", "us-east-1", "ap-northeast-1", "us-west-2", "us-west-1", "ap-southeast-1", "ap-southeast-2",
 		 "eu-central-1", "us-gov-west-1", "us-gov-east-1", "ap-northeast-2", "ap-south-1", "us-east-2", "ca-central-1", "eu-west-2", "eu-west-3",
-     "ap-northeast-3", "eu-north-1"]
+     "ap-northeast-3", "eu-north-1", "ap-east-1"]
 
       regions.each do |name|
         @_regions[name] = Region.new(name)
